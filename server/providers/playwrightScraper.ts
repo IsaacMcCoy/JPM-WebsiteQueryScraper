749fb583fcbe095622799f4e55d3fe11c6724a7c
Isaac McCoy <isaacmccoy@jpm-co.com>
2026-08-03 12:04:26 -0500

    connect to playwright provider and add logs for clarity

--- server/providers/playwrightScraper.ts
+++ server/providers/playwrightScraper.ts
@@ -3,7 +3,6 @@ import type { Browser } from "playwright"
 
 let browser: Browser | null = null
 
-
 async function getBrowser() {
   if (!browser) {
     browser = await chromium.launch({
@@ -13,7 +12,7 @@ async function getBrowser() {
   return browser
 }
 
-export async function playwrightProvider(url: string) {
+export async function playwrightProvider(url: string): Promise<string> {
 
   const browser = await getBrowser()
   const page = await browser.newPage()
@@ -21,21 +20,58 @@ export async function playwrightProvider(url: string) {
   await page.route("**/*", route => {
     const type = route.request().resourceType()
 
-    if (["image", "font", "media", "stylesheet"].includes(type)) {
+    if (["image", "font", "media"].includes(type)) {
       route.abort()
     } else {
       route.continue()
     }
   })
 
-  await page.goto(url, {
-    waitUntil: "domcontentloaded",
-    timeout: 30000
-  })
+  try {
+    await page.goto(url, {
+      waitUntil: "domcontentloaded",
+      timeout: 30000
+    })
+  } catch (err) {
+    console.error(err)
+    await page.close()
+    return ""
+  }
 
-  const html = await page.content()
+  try {
+    
+    // Required code for awaiting the page to load the gold prices 
+    // await page
+    //   .locator("#goldchange .tick-value.price-value .gpoticker-price")
+    //   .filter({ hasText: /\d/ })
+    //   .waitFor()
 
-  await page.close()
+    // Optional code for displaying the number of things found
+    // const count = await page
+    //   .locator("#goldchange .tick-value.price-value .gpoticker-price")
+    //   .count()
+    // console.log("Found:", count)
 
-  return html
+    // Optional code for displaying price in the console for verification
+    // let price = await page
+    //   .locator("#goldchange tick-value.price-value .gpoticker-price")
+    //   .first()
+    //   .textContent()
+    // console.log(price)
+
+    const html = await page.content()
+
+    await page.close()
+    if (html) {
+      return html
+    } else {
+      console.log("Playwright Extraction: No HTML")
+      return ""
+    }
+
+  } catch (err) {
+    console.log(err)
+    await page.close()
+    return ""
+  }
 }
